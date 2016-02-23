0da2b52c8141284f42c41181119aeec1fe695532
Simon Woerpel <simon.woerpel@medienrevolte.de>
2016-02-23 14:52:05 +0100

    oh-my-zsh: auto activate virtualenvs in same directory

--- oh-my-zsh/custom/virtualenv-auto-activate.sh
+++ oh-my-zsh/custom/virtualenv-auto-activate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # virtualenv-auto-activate.sh
-# 
+#
 # Installation:
 #   Add this line to your .bashrc or .bash-profile:
 #
@@ -25,7 +25,7 @@
 #
 #   The virtualenv will be activated automatically when you enter the directory.
 _virtualenv_auto_activate() {
-    if [ -e "../venv" ]; then
+    if [ -e "./venv" ]; then
         # Check to see if already activated to avoid redundant activating
         if [ "$VIRTUAL_ENV" != "$(pwd -P)/venv" ]; then
             # fits to my naming conventions:
@@ -34,7 +34,7 @@ _virtualenv_auto_activate() {
             # echo Activating virtualenv \"$_VENV_NAME\"...
             echo Activating virtualenv...
             VIRTUAL_ENV_DISABLE_PROMPT=1
-            source ../venv/bin/activate
+            source ./venv/bin/activate
             _OLD_VIRTUAL_PS1="$PS1"
             PS1="($_VENV_NAME)$PS1"
             export PS1
