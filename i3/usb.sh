1b7be568f688351f55e8fc1eb67b75b0e03c99a4
Simon Wörpel <simon.woerpel@medienrevolte.de>
2016-10-05 08:32:34 +0200

    Some specific tweaks for old thinkpad

--- i3/usb.sh
+++ i3/usb.sh
@@ -4,19 +4,20 @@
 # if mount, start ranger to ~/.usb1
 
 if findmnt /dev/sdb1 > /dev/null; then
+    sync
     umount /dev/sdb1
     notify-send 'UNMOUNT: usb' -t 500;
 elif findmnt /dev/sdb > /dev/null; then
+    sync
     umount /dev/sdb
     notify-send 'UNMOUNT: usb' -t 500;
 else
     if mount /dev/sdb1; then
         notify-send 'MOUNT: usb' -t 500
-        xterm -e vifm ~/.usb1;
-        # xterm; cd ~/.usb1
+        # xterm -e vifm ~/.usb1;
     elif mount /dev/sdb; then
         notify-send 'MOUNT: usb' -t 500
-        xterm -e vifm ~/.usb1;
+        # xterm -e vifm ~/.usb1;
     else
         notify-send -u critical 'MOUNT: usb failed' -t 500
     fi
