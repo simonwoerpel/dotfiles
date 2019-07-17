94d37ebb24fc50b7c37f4551f8493430e04925a4
Simon Wörpel <simon.woerpel@medienrevolte.de>
2019-07-17 16:44:50 +0200

    i3: use scripts for xrandr stuff

---
+++ screenlayout/_office.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output DP3 --off --output eDP1 --primary --mode 2560x1440 --pos 1920x0 --rotate normal --output DP1 --off --output DP2 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output DP3-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP3-3 --off --output DP3-2 --mode 1920x1080 --pos 4480x0 --rotate left
---
+++ screenlayout/solo.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+xrandr --output eDP1 --primary --mode 2560x1440
+xrandr --output DP3-1 --off
+xrandr --output DP3-2 --off
