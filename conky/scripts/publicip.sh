b516becab4f8cf90136267e0fd612cabec037f0b
Simon Wörpel <simon.woerpel@medienrevolte.de>
2020-12-16 18:01:30 +0100

    cleanup

--- conky/scripts/publicip.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-wget http://checkip.dyndns.org/ -q -O - | grep -Eo '\<[[:digit:]]{1,3}(\.[[:digit:]]{1,3}){3}\>'
--- screenlayout/office.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-xrandr --output eDP1 --primary --mode 2560x1440
-xrandr --output DP3-1 --mode 1920x1080 --left-of eDP1
-xrandr --output DP3-2 --mode 1920x1080 --right-of eDP1 --rotate left
