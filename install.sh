58fdceb55ab91b813d44a6551637dbe8313fe59e
Simon Wörpel <simon.woerpel@medienrevolte.de>
2020-12-17 12:04:12 +0100

    Cleanup install script

--- install.sh
+++ install.sh
@@ -1,43 +1,7 @@
 #!/bin/bash
-############################
-# .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
 
-########## Variables
-
-dir=~/.dotfiles                    # dotfiles directory
-olddir=~/.dotfiles_old             # old dotfiles backup directory
-files="vim conky i3"    # list of files/folders to symlink in homedir
-
-##########
-
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
-
-# change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
-echo "...done"
-
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~$olddir/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
-done
-
-# non-directory stuff
-
-mv ~/.xsession ~$olddir/
-mv ~/.Xresources ~$olddir/
-ln -s xorg/.xsession ~
-ln -s xorg/.Xresources ~
-
-mv ~/.aliases ~$olddir/
-mv ~/.zshrc ~$olddir/
-ln -s zsh/aliases ~/.aliases
-ln -s zsh/zshrc ~/.zshrc
+DOTFILES=~/.sys/dotfiles
+ln -s $DOTFILES/zsh/zshrc ~/.zshrc
+ln -s $DOTFILES/i3 ~/.i3
+ln -s $DOTFILES/config/compton.conf ~/.config/
+ln -s $DOTFILES/vim ~/.vim
